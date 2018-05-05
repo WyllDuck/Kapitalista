6bd6254ae66cbdd362c8d495db13cf79820ea49e
WyllDuck <martivalverde.98@gmail.com>
2018-05-05 18:51:35 +0200

    Django Website/ First Iteration/ v6.1

--- mysite/graph_view/views.py
+++ mysite/graph_view/views.py
@@ -21,7 +21,7 @@ HTML RENDER VARIABLES : subjects_jinja
 
 FORMAT EXAMPLE:
 
-        {'data':
+        {'data_graph_1':
            [[ 10, 12, 33, 32, 11, 2],
             [ 10, 12, 33, 32, 11, 2],
             [ 10, 12, 33, 32, 11, 2],
@@ -32,10 +32,11 @@ FORMAT EXAMPLE:
             [ 10, 12, 33, 32, 11, 2],
             [ 10, 12, 33, 32, 11, 2],
             [ 10, 12, 33, 32, 11, 2]],
-            'x_labels': "0-1,1-2,2-3,3-4,4-5,5-6,6-7,7-8,8-9,9-10",
-            'bar_labels': "x_labels, 5000-10000, 10000-12500, 12500-15000, 15000-20000, 20000-30000,+ 30000",
-            'title': 'TMM (Theory in Machines and Mechanism)',
-            'subjects_jinja': subjects_jinja})
+            'x_labels_graph_1': "0-1,1-2,2-3,3-4,4-5,5-6,6-7,7-8,8-9,9-10",
+            'bar_labels_graph_1': "x_labels, 5000-10000, 10000-12500, 12500-15000, 15000-20000, 20000-30000,+ 30000",
+            'title_graph_1': 'TMM (Theory in Machines and Mechanism)',
+            'subjects_jinja': get_input_var(int(subject_code)),
+            'current_subject': get_current_subject(int(subject_code))}
 """
 def graph_1 (request, subject_code):
 
@@ -93,12 +94,12 @@ HTML RENDER VARIABLES : incomes, incomes_input, tabulated_incomes, rest
 
 FORMAT EXAMPLE:
 
-       {'data': [6.5, 5.7, 5.4, 5.1, 4.7, 4.3],
-           'bar_labels': "5000-10000,10000-12500,12500-15000,15000-20000,20000-30000,+ 30000",
-           'incomes': parser_html,
-           'incomes_input': parser_html[:-1] if (rest and parser_html != 'None') else parser_html,
-           'tabulated_incomes': parser_pandas,
-           'rest': 1 if rest else 0 })
+       {'data_graph_2': [6.5, 5.7, 5.4, 5.1, 4.7, 4.3],
+            'bar_labels_graph_2': "5000-10000,10000-12500,12500-15000,15000-20000,20000-30000,+ 30000",
+            'incomes': parser_html,
+            'incomes_input': parser_html[:-1] if (rest and parser_html != 'None') else parser_html,
+            'tabulated_incomes': parser_pandas,
+            'rest': 1 if rest else 0 }
 """
 def graph_2 (request, incomes):
     # Parse incomes variable:
@@ -140,21 +141,21 @@ HTML RENDER VARIABLES : incomes, incomes_input, tabulated_incomes, rest
 
 FORMAT EXAMPLE:
 
-         {'data':
-            [[5, 5.5, 6.7, 6.1, 5.5, 4.5],
-             [5.6, 6.0, 7.0, 6.6, 6.5, 4.6],
-             [5.8, 6.1, 6.8, 6.3, 7.1, 5.0],
-             [5.5, 4.8, 6.5, 6.4, 7.2, 4.5],
-             [5.7, 5.9, 6.4, 6.3, 7.6, 6.0],
-             [6.0, 6.5, 6.0, 6.0, 6.5, 4.9],
-             [6.1, 6.0, 6.6, 6.0, 6.3, 5.1],
-             [5.6, 6.3, 6.7, 5.7, 6.1, 5.4]],
-             'x_labels': 'Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8',
-             'line_labels': '5000-10000,10000-12500,12500-15000,15000-20000,20000-30000,+ 30000',
-             'incomes': parser_html,
-             'incomes_input': parser_html[:-1] if (rest and parser_html != 'None') else parser_html,
-             'tabulated_incomes': parser_pandas,
-             'rest': 1 if rest else 0 })
+        {'data_graph_3':
+           [[5, 5.5, 6.7, 6.1, 5.5, 4.5],
+            [5.6, 6.0, 7.0, 6.6, 6.5, 4.6],
+            [5.8, 6.1, 6.8, 6.3, 7.1, 5.0],
+            [5.5, 4.8, 6.5, 6.4, 7.2, 4.5],
+            [5.7, 5.9, 6.4, 6.3, 7.6, 6.0],
+            [6.0, 6.5, 6.0, 6.0, 6.5, 4.9],
+            [6.1, 6.0, 6.6, 6.0, 6.3, 5.1],
+            [5.6, 6.3, 6.7, 5.7, 6.1, 5.4]],
+            'x_labels_graph_3': 'Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8',
+            'line_labels_graph_3': '5000-10000,10000-12500,12500-15000,15000-20000,20000-30000,+ 30000',
+            'incomes': parser_html,
+            'incomes_input': parser_html[:-1] if (rest and parser_html != 'None') else parser_html,
+            'tabulated_incomes': parser_pandas,
+            'rest': 1 if rest else 0 }
 """
 def graph_3 (request, incomes):
     # Parse incomes variable:
@@ -182,6 +183,44 @@ def graph_3 (request, incomes):
 
 """
 Note: This view is a general view of the three graphic at the same time for a given postal code
+
+This is a combination of the four graphic above   ( ""graph_1"",
+                                                    ""graph_2"",
+                                                    ""graph_3"")
+
+
+FORMAT EXAMPLE:
+
+    'data_graph_1':
+           [[ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2],
+            [ 10, 12, 33, 32, 11, 2]],
+        'x_labels_graph_1': "0-1,1-2,2-3,3-4,4-5,5-6,6-7,7-8,8-9,9-10",
+        'bar_labels_graph_1': "x_labels, 5000-10000, 10000-12500, 12500-15000, 15000-20000, 20000-30000,+ 30000",
+        'title_graph_1': 'TMM (Theory in Machines and Mechanism)',
+        'data_graph_2': [6.5, 5.7, 5.4, 5.1, 4.7, 4.3],
+        'bar_labels_graph_2': "5000-10000,10000-12500,12500-15000,15000-20000,20000-30000,+ 30000",
+        'data_graph_3':
+           [[5, 5.5, 6.7, 6.1, 5.5, 4.5],
+            [5.6, 6.0, 7.0, 6.6, 6.5, 4.6],
+            [5.8, 6.1, 6.8, 6.3, 7.1, 5.0],
+            [5.5, 4.8, 6.5, 6.4, 7.2, 4.5],
+            [5.7, 5.9, 6.4, 6.3, 7.6, 6.0],
+            [6.0, 6.5, 6.0, 6.0, 6.5, 4.9],
+            [6.1, 6.0, 6.6, 6.0, 6.3, 5.1],
+            [5.6, 6.3, 6.7, 5.7, 6.1, 5.4]],
+        'x_labels_graph_3': 'Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8',
+        'line_labels_graph_3': '5000-10000,10000-12500,12500-15000,15000-20000,20000-30000,+ 30000',
+        'postal_code': postal_code,
+        'location': 'PS Sant Joan 154 6º2'}
+
 """
 def general (request, postal_code):
 
